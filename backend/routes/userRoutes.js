58cb44a4274a00132c376ca669bc2dc3424c0623
avicos <adapaavinash612@gmail.com>
2025-08-09 19:13:09 +0530

    Task Management api complete

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express= require("express");
+const { adminOnly, protect } = require("../middlewares/authMiddleware");
+const {getUserById, getUsers, deleteUser} = require("../controllers/userController")
+const router = express.Router();
+
+router.get("/", protect, adminOnly, getUsers);
+router.get("/:id", protect, getUserById);
+
+
+module.exports = router;
